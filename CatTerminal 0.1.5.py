77398be016fe6dc34b77a40601561f61ea72ab8f
Cat <115361173+Catleooff@users.noreply.github.com>
2023-03-09 17:04:59 +0200

    новые комманды

---
+++ CatTerminal 0.1.5.py
@@ -0,0 +1,21 @@
+import cmd
+
+class MyTerminal(cmd.Cmd):
+    intro = "Welcome to CatTerminal! Type 'help' to see the available commands."
+    prompt = "> "
+    
+    def do_hello(self, arg):
+        print("Hello, world!")
+        
+    def do_exit(self, arg):
+        print("Пока")
+        return True
+        
+    def do_smile(self, arg):
+        print(")))))))))))))))))))))))))))))))))")
+        
+    def do_meow(self, arg):
+        print("мяу")
+        
+if __name__ == '__main__':
+    MyTerminal().cmdloop()
